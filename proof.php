2bf856948eaed15c7e209e973d3fb70838871261
Hayden Greer <haydengreer@utexas.edu>
2017-06-27 12:16:53 -0400

    Coding environment is ready to go

--- proof.php
+++ proof.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Landing Page</title>
+    <title>Proof Coil Works</title>
 
     <!-- Bootstrap -->
     <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- This page's style sheet -->
-    <link href="styles/index.css" rel="stylesheet">
+    <link href="styles/proof.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -23,8 +23,7 @@
 
 
   <body>
-    <h1>Hello, world!</h1>
-    <a href="/product.php">Product!</a>
+    <h1>Proof Coil Works</h1>
 
 
 
