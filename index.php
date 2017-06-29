7ffbc84baf8c27881a059883fc0871bda753da35
Nicolas Perlick <nicolasperlick@Nicolass-iMac.local>
2017-06-28 23:38:43 -0400

    coverr video success

--- index.php
+++ index.php
@@ -10,8 +10,11 @@
     <!-- Bootstrap -->
     <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 
+
     <!-- This page's style sheet -->
     <link href="styles/index.css" rel="stylesheet">
+    <!-- Mobile Friendly-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -23,9 +26,33 @@
 
 
   <body>
-    <h1>Hello, world!</h1>
-    <a href="/product.php">Product!</a>
+    <!--<div class="row">-->
+      <div class="col-lg-12">
+        <div class="loginButtons">
+        <!--Figure out how to make button link up-->
+            <button type="button" id="loginButton" class="btn btn-outline-primary">Login</button>
+            <button type="button" id="signupButton" class="btn btn-outline-primary">Sign in</button>
+        </div>
+        <div class="homepage-hero-module">
+            <div class="video-container">
+                <div class="filter"></div>
+                <video autoplay loop class="fillWidth">
+                    <source src="pencil_down/pencil_down.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+                    <source src="pencil_down/pencil_down.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
+                </video>
+                <div class="poster hidden">
+                    <img src="pencil_down/pencil_down.jpg" alt="">
+                </div>
+            </div>
+        </div>
+      </div>
+    <!--</div>-->
 
+
+
+      <!-- jQuery -->
+      
+    <script src="js/jquery.js"></script>
 
 
 
@@ -33,6 +60,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
   </body>
 
 
